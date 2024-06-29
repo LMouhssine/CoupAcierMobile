69204454aa10b4962aa62ea64625921f83e0fbd9
Mouhssine Lakhili <mouhssine.lakhili@yahoo.com>
2024-06-29 23:33:13 +0200

    Configuration MySQL

--- server.js
+++ server.js
@@ -15,16 +15,21 @@ const userRoutes = require('./backend/routes/userRoutes');
 
 app.use('/users', userRoutes);
 
+// Logs pour vérifier les valeurs des variables d'environnement
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PWD:', process.env.DB_PWD);
 console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('PORT_NUMBER:', process.env.PORT_NUMBER);
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock' // Spécifiez le chemin du socket pour MAMP
 });
 
 connection.connect((err) => {
