9ee9a99e2e99d07960255b43e9482071685ac7b9
Mouhssine Lakhili <mouhssine.lakhili@yahoo.com>
2024-07-18 23:05:24 +0200

    Navigation

---
+++ app/(tabs)/types.ts
@@ -0,0 +1,16 @@
+export type RootStackParamList = {
+    HomeScreen: undefined;
+    ProductPage: { productName: string };
+    Notifications: undefined;
+    TypeScreen: undefined;
+    Login: undefined;
+    SignupScreen: undefined;
+    CartScreen: undefined;
+    FavoritesScreen: undefined;
+    OrdersScreen: undefined;
+    ProfileScreen: undefined;
+    RegistrationScreenPart: undefined;
+    RegistrationScreenPro: undefined;
+    UserScreen: undefined;
+  };
+  
